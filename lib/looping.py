797e71de95c2255ffc243a3660399180f055b7f6
Mubarak-baka <Nassibmubarack@gmail.com>
2024-11-27 21:10:03 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+    countdown -=1 
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers=[x**2 for x in int_list]
+    return "squared_intergers"
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for z in (1,101):
+        if z % 3 == 0 and z % 5 == 0:
+            print("FizzBuzz")
+        elif z % 3 == 0 :
+            print("Fizz")
+        elif z  % 5 == 0:
+            print("Buzz")
+    
